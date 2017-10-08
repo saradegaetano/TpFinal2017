a21d66990bd8733f44a637743346083b8e5dfa04
saradegaetano <32110470+saradegaetano@users.noreply.github.com>
2017-10-08 13:05:49 -0300

    Create interfaceTarjeta.php

---
+++ src/interfaceTarjeta.php
@@ -0,0 +1,6 @@
+interface Tarjeta {
+ public function pagar(Transporte $transporte, $fecha_y_hora);
+ public function recargar($monto);
+ public function saldo();
+ public function viajesRealizados();
+}
