e97fb45e39c85a2a566346713eb5ff0b3156d2ed
saradegaetano <32110470+saradegaetano@users.noreply.github.com>
2017-10-15 11:15:31 -0300

    test para cargar saldo

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -11,6 +11,7 @@ class EstacionTest extends TestCase {
      */
     public function testSaldoCero() {
         $tarjeta = new Tarjeta;
-        $this->assertEquals($tarjeta->saldo(), 0);
+        $tarjeta->cargarSaldo ( 1000 )
+        $this->assertEquals( $tarjeta->saldo(), 1208 );
     }
 }
