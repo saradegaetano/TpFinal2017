08d0c6ffa0b5eacb7c4b66acc57c5177a090a913
saradegaetano <32110470+saradegaetano@users.noreply.github.com>
2017-10-15 11:10:21 -0300

    funcion cargar saldo y cambios necesarios

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -3,8 +3,27 @@
 namespace TpFinal;
 
 class Tarjeta implements Tarjeta {
-
-    public function saldo() {
-        return 0;
+    protected $saldo;
+    
+    function __construct () {
+        $this->saldo = 0;
+    }
+    public function saldo () {
+        return $this->saldo;
+    }
+    
+    public function cargarSaldo ( $monto ) {
+		if ( $monto < 332 ) {
+			$this->saldo += $monto;
+			return;
+		}
+		elseif ( $monto < 624 ) {
+			$this->saldo += 388;
+			cargarSaldo ( $monto - 332 );
+		}
+		elseif ( $monto >= 624 ) {
+			$this->saldo += 776;
+			cargarSaldo ( $monto - 624 );
+		}
     }
 }
