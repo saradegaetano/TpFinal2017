f4da39f28f95c918385f72e170d941cf77ade13b
saradegaetano <32110470+saradegaetano@users.noreply.github.com>
2017-10-20 09:59:30 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -13,4 +13,14 @@ class EstacionTest extends TestCase {
         $tarjeta = new Tarjeta;
         $this->assertEquals( $tarjeta->saldo(), 0 );
     }
+    public function testSaldoCincuenta() {
+        $tarjeta1 = new Tarjeta;
+        $tarjeta1->cargarSaldo( 50 );
+        $this->assertEquals( $tarjeta1->saldo(), 50 );
+    }
+    public function testSaldoTrescientos() {
+        $tarjeta2 = new Tarjeta;
+        $tarjeta2->cargarSaldo( 332 );
+        $this->assertEquals( $tarjeta2->saldo(), 388 );
+    }
 }
