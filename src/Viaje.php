f8ba599f8cfda88ddac88f02fc104a37d7cfb657
saradegaetano <32110470+saradegaetano@users.noreply.github.com>
2017-10-24 17:21:27 -0300

    Actualizacion funcion __construct

--- src/Viaje.php
+++ src/Viaje.php
@@ -7,6 +7,8 @@ class Viaje {
 	protected $tipo;
 	protected $monto;
 	protected $transporte;
+	protected $precioC = 9,70;
+	protected $precioB = 12,45;
 	
 	function __construct ( $tarjeta, Transporte $transporte ) {
 		$this->fecha = date( "Y/m/d" );
@@ -15,14 +17,10 @@ class Viaje {
 		$this->tipo = $tarjeta->franquicia;
 		
 		if ( is_a ( $this->transporte , Bici ) ) {
-			$this->monto = 12;
-		}
-		elseif ( is_a ( $this->transporte , Colectivo ) {
 			switch ( $this->tipo ) {
 			case "comun":
-				if ( $tarjeta->saldo >= 9,7 ) {
-					$tarjeta->saldo -= 9,7;
-					$this->monto = 9,7;
+				if ( $tarjeta->saldo >= $this->precioB {
+					$this->monto = $this->precioB;
 				}
 				elseif ($tarjeta->viajeplus <= 1) {
 					$tarjeta->viajeplus += 1;		
@@ -34,9 +32,40 @@ class Viaje {
 				break;
 					
 			case "estudiantil":
-				if ( $tarjeta->saldo >= 4,85 ) {
-					$tarjeta->saldo -= 4,85;
-					$this->monto = 4,85;
+				if ( $tarjeta->saldo >= round( ( $this->precioB / 2 ) , 2 ) ) ) {
+					$this->monto = round( ( $this->precioB / 2 ) , 2 ) ) );
+				}
+				elseif ($tarjeta->viajeplus <= 1) {
+					$tarjeta->viajeplus += 1;		
+				}
+				else {
+					echo "No tiene saldo suficiente y ya utilizo los dos viajes plus<br>";
+					// aca habria que meter algo de que no se puede hacer el viaje
+				}
+				break;
+			case "total":
+					$this->monto = 0;
+				break;
+			}
+		}
+		elseif ( is_a ( $this->transporte , Colectivo ) {
+			switch ( $this->tipo ) {
+			case "comun":
+				if ( $tarjeta->saldo >= $this->precioC ) {
+					$this->monto = $this->precioC;
+				}
+				elseif ($tarjeta->viajeplus <= 1) {
+					$tarjeta->viajeplus += 1;		
+				}
+				else {
+					echo "No tiene saldo suficiente y ya utilizo los dos viajes plus<br>";
+					// aca habria que meter algo de que no se puede hacer el viaje
+				}
+				break;
+					
+			case "estudiantil":
+				if ( $tarjeta->saldo >= round( ( $this->precioC / 2 ) , 2 ) ) ) ) {
+					$this->monto = round( ( $this->precioC / 2 ) , 2 ) ) ) );
 				}
 				elseif ($tarjeta->viajeplus <= 1) {
 					$tarjeta->viajeplus += 1;		
@@ -50,5 +79,6 @@ class Viaje {
 					$this->monto = 0;
 				break;
 		}
+		$tarjeta->saldo -= $this->monto;
 	}
 }
