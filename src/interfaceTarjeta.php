3fee656104d39de1b9496e3578997058fde7e7b9
Sara Degaetano <32110470+saradegaetano@users.noreply.github.com>
2017-10-28 18:06:18 -0300

    no hace falta fecha y hora

--- src/interfaceTarjeta.php
+++ src/interfaceTarjeta.php
@@ -3,7 +3,7 @@
 namespace TpFinal;
 
 interface interfaceTarjeta {
- public function pagar(Transporte $transporte, $fecha_y_hora);
+ public function pagar(Transporte $transporte);
  public function cargarSaldo($monto);
  public function saldo();
  public function viajesRealizados();
