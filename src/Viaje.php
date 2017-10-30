b52d4f8764edbb3cc7d9762827b4377fa02ff224
Sara Degaetano <32110470+saradegaetano@users.noreply.github.com>
2017-10-30 20:23:47 -0300

    cosas para bici

--- src/Viaje.php
+++ src/Viaje.php
@@ -73,68 +73,45 @@ class Viaje {
 			
 		
 		if ( is_a ( $this->transporte , Bici ) ) {
-			switch ( $this->tipo ) {
-			case "comun":
-				if ( $tarjeta->saldo >= $this->precioB {
-					$this->monto = $this->precioB;
+			$diaSiguiente = strtotime ( '+1 day' , strtotime ( $anterior->fecha ) );
+			$diaSiguiente = date ( 'Y/m/j' , $diaSiguiente );
+			if($this->fecha <= $diaSiguiente && $this->horaActual <= $anterior->horaActual) {
+				$this->monto = 0;
+			}
+			else {
+				switch ( $this->tipo ) {
+				case "comun":
+					if ( $tarjeta->saldo >= $this->precioB {
+						$this->monto = $this->precioB;
+					}
+					elseif ($tarjeta->viajeplus <= 1) {
+						$tarjeta->viajeplus += 1;		
+					}
+					else {
+						echo "No tiene saldo suficiente y ya utilizo los dos viajes plus<br>";
+						// aca habria que meter algo de que no se puede hacer el viaje
+					}
+					break;
+
+				case "estudiantil":
+					if ( $tarjeta->saldo >= round( ( $this->precioB / 2 ) , 2 ) )  {
+						$this->monto = round( ( $this->precioB / 2 ) , 2 ) ;
+					}
+					elseif ($tarjeta->viajeplus <= 1) {
+						$tarjeta->viajeplus += 1;		
+					}
+					else {
+						echo "No tiene saldo suficiente y ya utilizo los dos viajes plus<br>";
+						// aca habria que meter algo de que no se puede hacer el viaje
+					}
+					break;
+				case "total":
+						$this->monto = 0;
+					break;
 				}
-				elseif ($tarjeta->viajeplus <= 1) {
-					$tarjeta->viajeplus += 1;		
-				}
-				else {
-					echo "No tiene saldo suficiente y ya utilizo los dos viajes plus<br>";
-					// aca habria que meter algo de que no se puede hacer el viaje
-				}
-				break;
-					
-			case "estudiantil":
-				if ( $tarjeta->saldo >= round( ( $this->precioB / 2 ) , 2 ) )  {
-					$this->monto = round( ( $this->precioB / 2 ) , 2 ) ;
-				}
-				elseif ($tarjeta->viajeplus <= 1) {
-					$tarjeta->viajeplus += 1;		
-				}
-				else {
-					echo "No tiene saldo suficiente y ya utilizo los dos viajes plus<br>";
-					// aca habria que meter algo de que no se puede hacer el viaje
-				}
-				break;
-			case "total":
-					$this->monto = 0;
-				break;
 			}
 		}
-		elseif ( is_a ( $this->transporte , Colectivo ) {
-			switch ( $this->tipo ) {
-			case "comun":
-				if ( $tarjeta->saldo >= $this->precioC ) {
-					$this->monto = $this->precioC;
-				}
-				elseif ($tarjeta->viajeplus <= 1) {
-					$tarjeta->viajeplus += 1;		
-				}
-				else {
-					echo "No tiene saldo suficiente y ya utilizo los dos viajes plus<br>";
-					// aca habria que meter algo de que no se puede hacer el viaje
-				}
-				break;
-					
-			case "estudiantil":
-				if ( $tarjeta->saldo >= round( ( $this->precioC / 2 ) , 2 ) ) ) ) {
-					$this->monto = round( ( $this->precioC / 2 ) , 2 ) ) ) );
-				}
-				elseif ($tarjeta->viajeplus <= 1) {
-					$tarjeta->viajeplus += 1;		
-				}
-				else {
-					echo "No tiene saldo suficiente y ya utilizo los dos viajes plus<br>";
-					// aca habria que meter algo de que no se puede hacer el viaje
-				}
-				break;
-			case "total":
-					$this->monto = 0;
-				break;
-		}
+		
 		$tarjeta->saldo -= $this->monto;
 	}
 }
