8fabe7fee2cf0562e3498780f39204a5fd43e78a
Sara Degaetano <32110470+saradegaetano@users.noreply.github.com>
2017-10-30 10:28:42 -0300

    faltaba construct

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -4,6 +4,10 @@ namespace TpFinal;
 class Colectivo {
 	protected $linea;
 	
+	function __construct ($linea) {
+		$this->linea = $linea;
+	}
+	
 	public function linea () {
 		return $this->linea;
 	}
