2b953297ced0b3130a8884e1103ba1da11639e95
Sara Degaetano <32110470+saradegaetano@users.noreply.github.com>
2017-10-30 10:27:35 -0300

    imprimo cuando se crea un boleto

--- src/Boleto.php
+++ src/Boleto.php
@@ -13,5 +13,12 @@ class Boleto extends Viaje {
 		$this->saldo = $viaje->tarjeta->saldo();
 		$this->linea = $viaje->transporte->linea();
 		$this->idTarjeta = $viaje->tarjeta->id();
+		
+		echo $this->fecha "<br>";
+		echo $this->hora "<br>";
+		echo $this->"tarjeta: "idTarjeta "<br>";
+		echo $this->"linea: "linea "<br>";
+		echo $this->"franquicia: "tipo "<br>";
+		echo $this->"saldo: "saldo "<br>";
 	}
 }
