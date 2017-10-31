49cc19cdb30d8571cb2e177fa12d17034e6c129f
Sara Degaetano <32110470+saradegaetano@users.noreply.github.com>
2017-10-31 17:52:32 -0300

    faltaban las id de las tarjetas

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -10,35 +10,35 @@ class TarjetaTest extends TestCase {
      * Comprueba que el saldo de una tarjeta nueva sea cero.
      */
     public function testSaldoCero() {
-        $tarjeta = new Tarjeta("comun");
+        $tarjeta = new Tarjeta( 1234, "comun" );
         $this->assertEquals( $tarjeta->saldo(), 0 );
     }
     public function testSaldoCincuenta() {
-        $tarjeta = new Tarjeta("comun");
+        $tarjeta = new Tarjeta( 1234, "comun");
         $tarjeta->cargarSaldo( 50 );
         $this->assertEquals( $tarjeta->saldo(), 50 );
     }
     public function testSaldoTrescientos() {
-        $tarjeta = new Tarjeta ("comun");
+        $tarjeta = new Tarjeta ( 1234, "comun");
         $tarjeta->cargarSaldo( 332 );
         $this->assertEquals( $tarjeta->saldo(), 388 );
     }
     public function testUnViaje() {
-	    $tarjeta = new Tarjeta ("comun");
+	    $tarjeta = new Tarjeta ( 1234, "comun");
 	    $tarjeta->cargarSaldo( 50 );
 	    $colectivo144 = new Colectivo ( 144 );
 	    $tarjeta->pagar($colectivo144);
 	    $this->assertEquals( $tarjeta->saldo(), (50-9.70) );
     }
 	 public function testDosViajes() {
-		$tarjeta = new Tarjeta ("comun");
+		$tarjeta = new Tarjeta ( 1234, "comun");
 		$tarjeta->cargarSaldo( 50 );
 		$colectivo144 = new Colectivo ( 144 );
 	    $tarjeta->pagar($colectivo144);
 	    $this->assertEquals( $tarjeta->saldo(), 50-9.70-9.70 );
     }
 	public function testViajeTransbordo() {
-		$tarjeta = new Tarjeta ("comun");
+		$tarjeta = new Tarjeta ( 1234, "comun");
 		$colectivo144 = new Colectivo ( 144 );
 		$tarjeta->cargarSaldo( 50 );
 	    $tarjeta->pagar($colectivo144);
@@ -47,7 +47,7 @@ class TarjetaTest extends TestCase {
 	    $this->assertEquals( $tarjeta->saldo(), 50-9.70-3.20 );
     }
 	public function testDosViajesTransbordo() {
-		$tarjeta = new Tarjeta ("comun");
+		$tarjeta = new Tarjeta ( 1234, "comun");
 		$colectivo103 = new Colectivo ( 103 );
 		$colectivo144 = new Colectivo ( 144 );
 		$tarjeta->cargarSaldo( 50 );
