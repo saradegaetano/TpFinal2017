d054158f475a970885f487b23be4bc1eea961400
Sara Degaetano <32110470+saradegaetano@users.noreply.github.com>
2017-11-01 16:48:08 -0300

    monto

--- src/Boleto.php
+++ src/Boleto.php
@@ -13,12 +13,14 @@ class Boleto extends Viaje {
 		$this->saldo = $viaje->tarjeta->saldo();
 		$this->linea = $viaje->transporte->linea();
 		$this->idTarjeta = $viaje->tarjeta->id();
+		$this->monto = $viaje->monto();
 		
 		echo $this->fecha .  "<br>";
 		echo $this->hora .  "<br>";
 		echo "tarjeta: " . $this->idTarjeta .  "<br>";
 		echo "linea: " . $this->linea  . "<br>";
-		echo "franquicia: " . $this->tipo .  "<br>";
+		echo "franquicia: " . $this->tipo .  "<br>";.
+		echo "monto: " . $this->monto .  "<br>";
 		echo "saldo: " . $this->saldo  . "<br>";
 	}
 }
