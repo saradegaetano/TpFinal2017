7dd16df7798321ab982744546ca8487d3d10f937
Sara Degaetano <32110470+saradegaetano@users.noreply.github.com>
2017-11-02 09:52:56 -0300

    tomaba siempre linea de transporte
    no estaba el caso de la bici

--- src/Boleto.php
+++ src/Boleto.php
@@ -11,14 +11,17 @@ class Boleto extends Viaje {
 		$this->hora = $viaje->hora;
 		$this->tipo = $viaje->tipo;
 		$this->saldo = $viaje->tarjeta->saldo();
-		$this->linea = $viaje->transporte->linea();
 		$this->idTarjeta = $viaje->tarjeta->id();
 		$this->monto = $viaje->monto();
+		if ( is_a ( $this->transporte , 'TpFinal\Colectivo' ) ) {
+			$this->linea = $viaje->transporte->linea();
+		}
+		else $this->linea = $viaje->transporte->id();
 		
 		echo $this->fecha .  "<br>";
 		echo $this->hora .  "<br>";
 		echo "tarjeta: " . $this->idTarjeta .  "<br>";
-		echo "linea: " . $this->linea  . "<br>";
+		echo "linea colectivo/id bici: " . $this->linea  . "<br>";
 		echo "franquicia: " . $this->tipo .  "<br>";
 		echo "monto: " . $this->monto .  "<br>";
 		echo "saldo: " . $this->saldo  . "<br>";
