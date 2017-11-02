52cdd4f45d97a4f4079530f4e5d488758f68b16e
Sara Degaetano <32110470+saradegaetano@users.noreply.github.com>
2017-11-02 10:29:17 -0300

    cambios en la estructura del programa

--- src/Viaje.php
+++ src/Viaje.php
@@ -35,9 +35,9 @@ class Viaje {
 		}
 
 		if ( is_a ( $this->transporte , 'TpFinal\Colectivo' ) ) {
-            if ( $viajes == NULL ) {
-                switch ( $this->tipo ) {
-                case "comun":
+           		if ( $viajes == NULL ) {
+                		switch ( $this->tipo ) {
+                		case "comun":
 					if ( $tarjeta->saldo() >= $this->precioC ) {
 						$this->monto = $this->precioC;
 					}
@@ -62,7 +62,7 @@ class Viaje {
                 }
             }
             elseif ( $anterior->transporte->linea( ) != $this->transporte->linea( ) ) {
-                if ( ( $this->hora > $this->seisam && $this->hora < $this->diezpm && ( $this->diaSemana == "Mon" || $this->diaSemana == "Tue" || $this->diaSemana == "Wed" || $this->diaSemana == "Thu" || $this->diaSemana == "Fri") ) || ( $this->hora > $this->seisam && $this->hora < $this->dospm && $this->diaSemana == "Sat" ) ) {
+                if ( (( $this->hora > $this->seisam && $this->hora < $this->diezpm && ( $this->diaSemana == "Mon" || $this->diaSemana == "Tue" || $this->diaSemana == "Wed" || $this->diaSemana == "Thu" || $this->diaSemana == "Fri") ) || ( $this->hora > $this->seisam && $this->hora < $this->dospm && $this->diaSemana == "Sat" )) || ( ( ( $this->horaActual - $anterior->horaActual ) / 60 ) < 90 && $this->cantTrasb != 1 ) ) {
                     if ( ( ( $this->horaActual - $anterior->horaActual ) / 60 ) < 60  && $this->cantTrasb != 1 ) {
                         if ( $this->tipo == "comun" ) {
                             if ( $tarjeta->saldo() >= round( ( $this->precioC / 3 ) , 1 ) ) {
@@ -83,27 +83,6 @@ class Viaje {
                     }
                 }
             }
-			elseif ( ( ( $this->horaActual - $anterior->horaActual ) / 60 ) < 90 && $this->cantTrasb != 1 ) {
-				if ( $this->tipo == "comun" ) {
-					if ( $tarjeta->saldo() >= round( ( $this->precioC / 3 ) , 1 ) ) {
-						$this->monto = round( ( $this->precioC / 3 ) , 1 );
-						$this->cantTrasb += 1;
-					}
-					else {
-						echo "No tiene saldo suficiente<br>";
-						$this->cantTrasb = 0;
-					}
-				}
-				elseif ( $this->tipo == "estudiantil" ) {
-					if ( $tarjeta->saldo() >= round( ( $this->precioC / 6 ) , 1 ) ) {
-						$this->monto = round( ( $this->precioC / 6 ) , 1 );
-					}
-					else {
-						echo "No tiene saldo suficiente<br>";
-						$this->cantTrasb = 0;
-					}
-				}
-			}
 		else {
             		switch ( $this->tipo ) {
                 	case "comun":
